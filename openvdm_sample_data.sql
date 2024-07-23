3b4c0b6a6dac438b6f9a2cfec503d9da9d3e42d5
Webb Pinner <webbpinner@gmail.com>
2024-07-23 11:34:15 -0400

    WIP on master

--- openvdm_sample_data.sql
+++ openvdm_sample_data.sql
@@ -69,13 +69,12 @@ CREATE TABLE `OVDM_CollectionSystemTransfers` (
 
 LOCK TABLES `OVDM_CollectionSystemTransfers` WRITE;
 /*!40000 ALTER TABLE `OVDM_CollectionSystemTransfers` DISABLE KEYS */;
-INSERT INTO `OVDM_CollectionSystemTransfers` VALUES
-  (1,'OpenRVDAS','OpenRVDAS (SSH Server)',0,'/data/sample_data/ssh_source/OpenRVDAS','OpenRVDAS',0,0,0,1,1,0,4,0,'','','','','','','','127.0.0.1','survey',1,NULL,'*','','',2,1,0,0),
-  (2,'XBT','XBT  (Authenticated SMB Share)',0,'XBT','XBT',0,0,0,1,1,0,3,0,'','','','//localhost/SampleAuthSource','survey','sample_smb_passwd','WORKGROUP','','',0,'','*{cruiseID}_XBT[0-9][0-9][0-9]_*','','',2,1,0,0),
-  (3,'EM302','EM302 Multibeam (Rsync Server)',0,'/EM302','EM302',0,0,0,1,1,0,2,0,'localhost/sample_data','survey','b4dPassword!','','','','','','',0,'','*','','',2,1,0,0),
-  (4,'CTD','SBE 911+ CTD (Local Directory)',0,'/data/sample_data/local_source/CTD','CTD ',0,0,0,1,1,0,1,0,'','','','','','','','','',0,'','*{cruiseID}_CTD[0-9][0-9][0-9]_*','','*decktest*',2,1,0,0),
-  (5,'Sealog','Sealog (Guest SMB Share)',0,'sealog','Sealog',0,0,0,1,1,0,3,0,'','','','//localhost/SampleAnonSource','guest','','WORKGROUP','','',0,'','*','','',2,1,0,0),
-  (6,'ROV_OpenRVDAS','OpenRVDAS collecting data for ROV',1,'/data/sample_data/local_source/OpenRVDAS','OpenRVDAS',0,0,0,1,1,0,1,0,'','','','','','','','','',0,'','*','','',2,1,0,0);
+INSERT INTO `OVDM_CollectionSystemTransfers` VALUES (1,'OpenRVDAS','OpenRVDAS (SSH Server)',0,'/vault/sample_data/ssh_source/OpenRVDAS','OpenRVDAS',0,0,0,1,1,0,4,0,'','','','','','','','127.0.0.1','survey',1,NULL,'*','','',2,1,0,0),
+(2,'XBT','XBT  (Authenticated SMB Share)',0,'XBT','XBT',0,0,0,1,1,0,3,0,'','','','//localhost/SampleAuthSource','survey','sample_smb_passwd','WORKGROUP','','',0,'','*{cruiseID}_XBT[0-9][0-9][0-9]_*','','',2,1,0,0),
+(3,'EM302','EM302 Multibeam (Rsync Server)',0,'/EM302','EM302',0,0,0,1,1,0,2,0,'localhost/sample_data','survey','b4dPassword!','','','','','','',0,'','*','','',2,1,0,0),
+(4,'CTD','SBE 911+ CTD (Local Directory)',0,'/vault/sample_data/local_source/CTD','CTD ',0,0,0,1,1,0,1,0,'','','','','','','','','',0,'','*{cruiseID}_CTD[0-9][0-9][0-9]_*','','*decktest*',2,1,0,0),
+(5,'Sealog','Sealog (Guest SMB Share)',0,'sealog','Sealog',0,0,0,1,1,0,3,0,'','','','//localhost/SampleAnonSource','guest','','WORKGROUP','','',0,'','*','','',2,1,0,0),
+(6,'ROV_OpenRVDAS','OpenRVDAS collecting data for ROV',1,'/vault/sample_data/local_source/OpenRVDAS','OpenRVDAS',0,0,0,1,1,0,1,0,'','','','','','','','','',0,'','*','','',2,1,0,0);
 /*!40000 ALTER TABLE `OVDM_CollectionSystemTransfers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,12 +87,15 @@ DROP TABLE IF EXISTS `OVDM_CruiseDataTransfers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `OVDM_CruiseDataTransfers` (
-  `cruiseDataTransferID` int unsigned NOT NULL AUTO_INCREMENT,
+  `cruiseDataTransferID` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` tinytext NOT NULL,
   `longName` text,
-  `transferType` int unsigned NOT NULL,
+  `skipEmptyDirs` int(1) unsigned NOT NULL DEFAULT '1',
+  `skipEmptyFiles` int(1) unsigned NOT NULL DEFAULT '1',
+  `syncToDest` int(1) unsigned NOT NULL DEFAULT '0',
+  `transferType` int(11) unsigned NOT NULL,
   `destDir` tinytext,
-  `localDirIsMountPoint` int unsigned NOT NULL DEFAULT '0',
+  `localDirIsMountPoint` int(1) unsigned NOT NULL DEFAULT '0',
   `rsyncServer` tinytext,
   `rsyncUser` tinytext,
   `rsyncPass` tinytext,
@@ -103,15 +105,15 @@ CREATE TABLE `OVDM_CruiseDataTransfers` (
   `smbDomain` tinytext,
   `sshServer` tinytext,
   `sshUser` tinytext,
-  `sshUseKey` int unsigned NOT NULL DEFAULT '0',
+  `sshUseKey` int(1) unsigned NOT NULL DEFAULT '0',
   `sshPass` tinytext,
-  `status` int unsigned NOT NULL DEFAULT '3',
+  `status` int(11) unsigned NOT NULL DEFAULT '3',
   `enable` tinyint(1) NOT NULL DEFAULT '0',
   `required` tinyint(1) NOT NULL DEFAULT '0',
-  `pid` int unsigned DEFAULT '0',
-  `bandwidthLimit` int unsigned NOT NULL DEFAULT '0',
-  `includeOVDMFiles` int unsigned NOT NULL DEFAULT '0',
-  `includePublicDataFiles` int unsigned NOT NULL DEFAULT '0',
+  `pid` int(11) unsigned DEFAULT '0',
+  `bandwidthLimit` int(10) unsigned NOT NULL DEFAULT '0',
+  `includeOVDMFiles` int(1) unsigned NOT NULL DEFAULT '0',
+  `includePublicDataFiles` int(1) unsigned NOT NULL DEFAULT '0',
   `excludedCollectionSystems` tinytext,
   `excludedExtraDirectories` tinytext,
   PRIMARY KEY (`cruiseDataTransferID`),
@@ -128,7 +130,12 @@ CREATE TABLE `OVDM_CruiseDataTransfers` (
 
 LOCK TABLES `OVDM_CruiseDataTransfers` WRITE;
 /*!40000 ALTER TABLE `OVDM_CruiseDataTransfers` DISABLE KEYS */;
-INSERT INTO `OVDM_CruiseDataTransfers` VALUES (1,'SSDW','Shoreside Data Warehouse',4,'/data/sample_data/ssdw',0,'','','','','','','','localhost','survey',1,NULL,2,1,1,0,128,0,0,'0','0'),(2,'Local_Copy','Cruise copy to local directory',1,'/data/sample_data/local_destination',0,'','','','','','','','','',0,'',2,1,0,0,0,0,0,'0','0'),(3,'Cruise_Auth_SMB','Cruise copy to authenticated SMB share',3,'/',0,'','','','//localhost/SampleAuthDestination','survey','sample_smb_passwd','WORKGROUP','','',0,'',2,1,0,0,0,0,0,'0','0'),(4,'Cruise_Anon_SMB','Cruise copy to anonymous SMB share',3,'/',0,'','','','//localhost/SampleAnonDestination','guest','','WORKGROUP','','',0,'',2,1,0,0,0,0,0,'0','0'),(5,'cruise_rsync','Cruise copy to rsync server',2,'/',0,'localhost/sample_dest','survey','b4dPassword!','','','','','','',0,'',2,1,0,0,0,0,0,'0','0'),(6,'cruise_ssh','Cruise copy to SSH server',4,'/data/sample_data/ssh_destination',0,'','','','','','','','localhost','survey',1,NULL,2,1,0,0,0,0,0,'0','0');
+INSERT INTO `OVDM_CruiseDataTransfers` VALUES (1,'SSDW','Shoreside Data Warehouse',1,1,0,4,'/vault/sample_data/ssdw',0,'','','','','','','','localhost','survey',1,NULL,2,1,1,0,128,0,0,'0','0'),
+(2,'Local_Copy','Cruise copy to local directory',1,1,0,1,'/vault/sample_data/local_destination',0,'','','','','','','','','',0,'',2,1,0,0,0,0,0,'0','0'),
+(3,'Cruise_Auth_SMB','Cruise copy to authenticated SMB share',1,1,0,3,'/',0,'','','','//localhost/SampleAuthDestination','survey','sample_smb_passwd','WORKGROUP','','',0,'',2,1,0,0,0,0,0,'0','0'),
+(4,'Cruise_Anon_SMB','Cruise copy to anonymous SMB share',1,1,0,3,'/',0,'','','','//localhost/SampleAnonDestination','guest','','WORKGROUP','','',0,'',2,1,0,0,0,0,0,'0','0'),
+(5,'cruise_rsync','Cruise copy to rsync server',1,1,0,2,'/',0,'localhost/sample_dest','survey','b4dPassword!','','','','','','',0,'',2,1,0,0,0,0,0,'0','0'),
+(6,'cruise_ssh','Cruise copy to SSH server',1,1,0,4,'/vault/sample_data/ssh_destination',0,'','','','','','','','localhost','survey',1,NULL,2,1,0,0,0,0,0,'0','0');
 /*!40000 ALTER TABLE `OVDM_CruiseDataTransfers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -157,11 +164,10 @@ CREATE TABLE `OVDM_ExtraDirectories` (
 
 LOCK TABLES `OVDM_ExtraDirectories` WRITE;
 /*!40000 ALTER TABLE `OVDM_ExtraDirectories` DISABLE KEYS */;
-INSERT INTO `OVDM_ExtraDirectories` VALUES
-  (1,'Transfer_Logs','Transfer Logs',0,'OpenVDM/TransferLogs',1,1),
-  (2,'Dashboard_Data','Dashboard Data',0,'OpenVDM/DashboardData',1,1),
-  (3,'From_PublicData','Files copied from PublicData share',0,'From_PublicData',1,1),
-  (4,'Tracklines','Cruise Tracklines',0,'Products/Tracklines',1,0);
+INSERT INTO `OVDM_ExtraDirectories` VALUES (1,'Transfer_Logs','Transfer Logs','OpenVDM/TransferLogs',1,1),
+(2,'Dashboard_Data','Dashboard Data','OpenVDM/DashboardData',1,1),
+(3,'From_PublicData','Files copied from PublicData share','From_PublicData',1,1),
+(4,'Tracklines','Cruise Tracklines','Products/Tracklines',1,0);
 /*!40000 ALTER TABLE `OVDM_ExtraDirectories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -193,7 +199,12 @@ CREATE TABLE `OVDM_ShipToShoreTransfers` (
 
 LOCK TABLES `OVDM_ShipToShoreTransfers` WRITE;
 /*!40000 ALTER TABLE `OVDM_ShipToShoreTransfers` DISABLE KEYS */;
-INSERT INTO `OVDM_ShipToShoreTransfers` VALUES (1,'DashboardData','Dashboard Data',1,0,2,'*',1,1),(2,'TransferLogs','Transfer Logs',1,0,1,'*',0,1),(3,'MD5Summary','MD5 Summary',1,0,0,'MD5_Summary.txt,MD5_Summary.md5 ',1,1),(4,'OVDM_Config','OpenVDM Configuration',1,0,0,'ovdmConfig.json',1,1),(5,'GGA_FILES','GGA Files',1,1,0,'*_GGA-*.txt',1,0),(6,'MET','Met raw files',3,1,0,'*MET-*.txt,*TSG_Raw-*.txt',1,0);
+INSERT INTO `OVDM_ShipToShoreTransfers` VALUES (1,'DashboardData','Dashboard Data',1,0,2,'*',1,1),
+(2,'TransferLogs','Transfer Logs',1,0,1,'*',0,1),
+(3,'MD5Summary','MD5 Summary',1,0,0,'MD5_Summary.txt,MD5_Summary.md5 ',1,1),
+(4,'OVDM_Config','OpenVDM Configuration',1,0,0,'ovdmConfig.json',1,1),
+(5,'GGA_FILES','GGA Files',1,1,0,'*_GGA-*.txt',1,0),
+(6,'MET','Met raw files',3,1,0,'*MET-*.txt,*TSG_Raw-*.txt',1,0);
 /*!40000 ALTER TABLE `OVDM_ShipToShoreTransfers` ENABLE KEYS */;
 UNLOCK TABLES;
 
